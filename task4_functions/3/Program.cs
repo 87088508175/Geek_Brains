7debd198d721b53dedb55b0232ab1eec9a310b39
Olzhas Sakenov <osakienov@bk.ru>
2023-03-25 22:54:19 +0600

    initialised

---
+++ task4_functions/3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArrayOutput
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = new int[] { 1, 2, 5, 7, 19, 6, 1, 33 };
+
+			Console.WriteLine("Элементы массива:");
+
+			foreach (int element in arr)
+			{
+				Console.WriteLine(element);
+			}
+		}
+	}
+}
