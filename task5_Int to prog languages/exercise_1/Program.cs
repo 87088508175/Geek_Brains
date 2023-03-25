c43c5fed2952a933ce95b5c8336abf86b39604b4
Olzhas Sakenov <osakienov@bk.ru>
2023-03-25 23:11:20 +0600

    initialised

---
+++ task5_Int to prog languages/exercise_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EvenNumbersInArray
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+
+			int[] arr = new int[10];
+
+			// Заполнение массива случайными трехзначными числами
+			for (int i = 0; i < arr.Length; i++)
+			{
+				arr[i] = random.Next(100, 1000);
+			}
+
+			Console.WriteLine("Массив из случайных трехзначных чисел:");
+			Console.WriteLine(string.Join(", ", arr));
+
+			int count = 0;
+
+			// Подсчет количества четных чисел в массиве
+			foreach (int element in arr)
+			{
+				if (element % 2 == 0)
+				{
+					count++;
+				}
+			}
+
+			Console.WriteLine($"Количество четных чисел в массиве: {count}");
+		}
+	}
+}
