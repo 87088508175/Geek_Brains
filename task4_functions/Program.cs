35c012559cd2184dd756a447f13bc7c3228b874c
Olzhas Sakenov <osakienov@bk.ru>
2023-03-25 22:32:27 +0600

    initialised

--- task4_functions/Program.cs
+++ task4_functions/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Введите число A:");
+		int a = Convert.ToInt32(Console.ReadLine()); // считываем число A
+
+		Console.WriteLine("Введите степень B:");
+		int b = Convert.ToInt32(Console.ReadLine()); // считываем степень B
+
+		int result = 1;
+		for (int i = 0; i < b; i++)
+		{
+			result *= a;
+		}
+
+		Console.WriteLine("{0} в степени {1} равно {2}", a, b, result);
+	}
+}
