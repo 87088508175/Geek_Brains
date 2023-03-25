ebb6380389d77e4ea460ed56b2ddedb66e0c9d63
Olzhas Sakenov <osakienov@bk.ru>
2023-03-25 22:51:00 +0600

    initialised

---
+++ task4_functions/2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumOfDigits
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Введите число: ");
+			string input = Console.ReadLine();
+
+			int sum = 0;
+
+			foreach (char digit in input)
+			{
+				if (char.IsDigit(digit))
+				{
+					sum += int.Parse(digit.ToString());
+				}
+			}
+
+			Console.WriteLine($"Сумма цифр числа {input}: {sum}");
+		}
+	}
+}
