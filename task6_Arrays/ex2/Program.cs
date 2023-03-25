557b0392a51d3f3818b33d8bf18b2a84685a9d6c
Olzhas Sakenov <osakienov@bk.ru>
2023-03-26 02:18:04 +0600

    initialised

---
+++ task6_Arrays/ex2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		// Запрос значений у пользователя
+		Console.Write("Введите значение k1: ");
+		double k1 = double.Parse(Console.ReadLine());
+
+		Console.Write("Введите значение b1: ");
+		double b1 = double.Parse(Console.ReadLine());
+
+		Console.Write("Введите значение k2: ");
+		double k2 = double.Parse(Console.ReadLine());
+
+		Console.Write("Введите значение b2: ");
+		double b2 = double.Parse(Console.ReadLine());
+
+		// Рассчитываем координаты точки пересечения
+		double x = (b2 - b1) / (k1 - k2);
+		double y = k1 * x + b1;
+
+		// Выводим результат
+		Console.WriteLine($"Точка пересечения: ({x}, {y})");
+
+		// Ждем, пока пользователь не нажмет Enter, чтобы закрыть консольное окно
+		Console.ReadLine();
+	}
+}
