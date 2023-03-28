5a51e6c39d8c5727ae0563a20fd4d53169a4f78a
Olzhas Sakenov <osakienov@bk.ru>
2023-03-28 19:33:58 +0600

    Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива

---
+++ task 8_2D arrays/ex_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int[,] arr = { { 3, 2, 1 }, { 6, 5, 4 }, { 9, 8, 7 } }; // исходный двумерный массив
+
+		int m = arr.GetLength(0); // количество строк
+		int n = arr.GetLength(1); // количество столбцов
+
+		// упорядочиваем элементы каждой строки по убыванию
+		for (int i = 0; i < m; i++)
+		{
+			// используем метод Array.Sort с обратным порядком сортировки
+			Array.Sort(arr, i * n, n, new Comparison<int>((x, y) => y.CompareTo(x)));
+		}
+
+		// выводим результаты на экран
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				Console.Write("{0} ", arr[i, j]);
+			}
+			Console.WriteLine();
+		}
+	}
+}
