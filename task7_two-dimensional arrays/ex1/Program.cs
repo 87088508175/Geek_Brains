a0ff665c220fab3ef75e2c20a2fbe0aaf34f6db1
Olzhas Sakenov <osakienov@bk.ru>
2023-03-28 19:02:04 +0600

    initialised

---
+++ task7_two-dimensional arrays/ex1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int m = 3; // количество строк
+		int n = 4; // количество столбцов
+		double[,] arr = new double[m, n]; // создаем двумерный массив
+
+		// заполняем массив случайными числами
+		Random rand = new Random();
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				arr[i, j] = rand.NextDouble();
+			}
+		}
+
+		// выводим массив на экран
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				Console.Write("{0:0.00} ", arr[i, j]);
+			}
+			Console.WriteLine();
+		}
+	}
+}
