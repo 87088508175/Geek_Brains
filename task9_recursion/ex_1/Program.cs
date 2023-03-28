67a29d1bc7a675a1d7dbb079b6c7cc8eb9e18283
Olzhas Sakenov <osakienov@bk.ru>
2023-03-28 20:06:21 +0600

    Напишите программу, которая выведет все натуральные числа в промежутке от N до 1

---
+++ task9_recursion/ex_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int N = 5; // задаем значение N
+
+		PrintNumbers(N); // вызываем функцию для вывода чисел
+	}
+
+	static void PrintNumbers(int n)
+	{
+		if (n > 0)
+		{ // проверяем, что число n больше 0
+			Console.WriteLine(n); // выводим число n
+			PrintNumbers(n - 1); // вызываем функцию рекурсивно для числа n-1
+		}
+	}
+}
